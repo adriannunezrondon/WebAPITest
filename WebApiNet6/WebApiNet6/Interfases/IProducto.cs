e8a618963866c30a3ceec4e649d9349da933243d
anrondon01091987 <anrondon01091987@outlook.com>
2022-12-01 21:33:45 -0300

    Poniendo valores de atributos que pueden ser nulos

--- WebApiNet6/WebApiNet6/Interfases/IProducto.cs
+++ WebApiNet6/WebApiNet6/Interfases/IProducto.cs
@@ -1,6 +1,4 @@
 ﻿
-
-
 using Microsoft.AspNetCore.Mvc;
 using WebApiNet6.Models;
 
--- WebApiNet6/WebApiNet6/Models/Producto.cs
+++ WebApiNet6/WebApiNet6/Models/Producto.cs
@@ -3,9 +3,9 @@
     public class Producto
     {
         public int ID { get; set; }
-        public string Nombre { get; set; }
-        public string Categorias { get; set; }
-        public string Descripcion { get; set; }
+        public string? Nombre { get; set; }
+        public string? Categorias { get; set; }
+        public string? Descripcion { get; set; }
         public float Precio { get; set; }
         public int Unidades { get; set; }
         public int EmpresaID { get; set; }
